48ebc0380f7babcb8791b5e76c41e4ed0edeffdc
Bmarouane2 <m.benhachem2@gmail.com>
2018-05-03 12:04:39 +0200

    adiing OU Approx Class
    adiing OU Approx Class

--- rBergomiProject_1.0/rBergomi/OUApprox.cs
+++ rBergomiProject_1.0/rBergomi/OUApprox.cs
@@ -4,10 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Meta.Numerics.Matrices;
+using Meta.Numerics.Analysis;
+using Meta.Numerics.Functions;
+using Meta.Numerics;
+using Meta.Numerics.SignalProcessing;
+
+
 namespace rBergomi
 {
-    class OUApprox
+    public class OUApproxVolterra
     {
-        //Test2
+        public Grid grid { get; set; }
+
+        //Gaussian Simulator
+        private GaussianSimulator simulator = new GaussianSimulator();
+        private double H;
+
+        //Structor
+        public OUApproxVolterra( Grid grid_, double H_ = 0.07)
+        {
+            grid = grid_;
+            H = H_;
+        }
+
+
     }
 }
+
